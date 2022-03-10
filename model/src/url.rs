917655f99b6a999d7d8583d852a8fba1b5d1decf
xaoirse <mxsadegh@gmail.com>
2022-03-10 14:11:17 +0330

    inhance url eq

--- model/src/url.rs
+++ model/src/url.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use super::*;
 use ::url as urlib;
 use chrono::{DateTime, Utc};
@@ -205,21 +207,49 @@ impl PartialOrd for Url {
 impl PartialEq for Url {
     fn eq(&self, other: &Self) -> bool {
         other.url[..urlib::Position::BeforePath] == self.url[..urlib::Position::BeforePath]
-            && match (other.url.path_segments(), self.url.path_segments()) {
-                (Some(a), Some(b)) => {
-                    let n = a
-                        .zip(b)
-                        .fold(0, |init, (a, b)| if a == b { init } else { init + 1 });
-                    n < 2
+            && (self.url.path() == other.url.path()
+                || match (other.url.path_segments(), self.url.path_segments()) {
+                    (Some(a), Some(b)) => {
+                        let a: HashSet<&str> = a
+                            .filter(|s| !s.is_empty() && s.parse::<usize>().is_err())
+                            .collect();
+                        let b: HashSet<&str> = b
+                            .filter(|s| !s.is_empty() && s.parse::<usize>().is_err())
+                            .collect();
+
+                        let c = &a - &b;
+                        let d = &b - &a;
+
+                        c.len() < 2 && d.len() < 2
+                    }
+                    (None, None) => true,
+                    _ => false,
+                })
+            && {
+                if self.url.path().ends_with(".css") && other.url.path().ends_with(".css") {
+                    return true;
                 }
-                (None, None) => true,
-                _ => false,
+                if self.url.path().ends_with(".js") && other.url.path().ends_with(".js") {
+                    return true;
+                }
+                true
             }
-            && other
-                .url
-                .query_pairs()
-                .map(|(k, _)| k)
-                .eq(self.url.query_pairs().map(|(k, _)| k))
+            && {
+                let a: HashSet<_> = other
+                    .url
+                    .query_pairs()
+                    .map(|(k, _)| k)
+                    .filter(|s| !s.is_empty())
+                    .collect();
+                let b: HashSet<_> = self
+                    .url
+                    .query_pairs()
+                    .map(|(k, _)| k)
+                    .filter(|s| !s.is_empty())
+                    .collect();
+
+                a == b
+            }
     }
 }
 
@@ -233,6 +263,11 @@ mod test {
         use std::str::FromStr;
 
         assert_eq!(
+            Url::from_str("https://app.lexoffice.de/voucher/settings/template/popover/voucher/settings/text/text/lib/css/libs.css?v=c644c27a4").unwrap(),
+            Url::from_str("https://app.lexoffice.de/voucher/settings/template/popover/voucher/settings/voucher/text/settings/voucher/settings/lib/css/libs.css?v=c644c27a4").unwrap()
+        );
+
+        assert_eq!(
             Url::from_str("https://memoryleaks.ir/tag/%d8%a2%d8%b3%db%8c%d8%a8-%d9%be%d8%b0%db%8c%d8%b1%db%8c-%d8%af%d8%b1-%d8%b1%d8%a8%d8%a7%d8%aa-%d9%87%d8%a7%db%8c-%d8%aa%d9%84%da%af%d8%b1%d8%a7%d9%85/feed/").unwrap(),
             Url::from_str("https://memoryleaks.ir/tag/%d8%af%d9%88%d8%b1-%d8%b2%d8%af%d9%86-%d9%85%da%a9%d8%a7%d9%86%db%8c%d8%b2%d9%85-%d8%aa%d8%b4%d8%ae%db%8c%d8%b5-%d8%af%d8%b3%d8%aa%da%af%d8%a7%d9%87-%d8%b1%d9%88%d8%aa-%d8%b4%d8%af%d9%87/feed/").unwrap()
         );
@@ -240,11 +275,6 @@ mod test {
         assert_eq!(
             Url::from_str("http://aktuelles.haufe.de/eagle_kp_webapp/login/login.action;jsessionid=2acc8603184240a22e4fa5fe3525").unwrap(),
             Url::from_str("http://aktuelles.haufe.de/eagle_kp_webapp/login/login.action;jsessionid=2b0107dc883c28019e877799d07e").unwrap()
-        );
-
-        assert_eq!(
-            Url::from_str("https://memoryleaks.ir/category/%D9%88%DB%8C%D8%AF%D8%A6%D9%88-%D8%A2%D9%85%D9%88%D8%B2%D8%B4%DB%8C/page/2/").unwrap(),
-            Url::from_str("https://memoryleaks.ir/category/%d9%88%db%8c%d8%af%d8%a6%d9%88-%d8%a2%d9%85%d9%88%d8%b2%d8%b4%db%8c/page/2/").unwrap()
         );
 
         assert_eq!(
@@ -268,6 +298,11 @@ mod test {
         assert_ne!(
             Url::from_str("https://a.com/a/b/c/").unwrap(),
             Url::from_str("https://a.com/a/d/e").unwrap()
+        );
+
+        assert_ne!(
+            Url::from_str("https://a.com/a/23/b").unwrap(),
+            Url::from_str("https://b.com/b/a/23/a/").unwrap()
         );
     }
 
